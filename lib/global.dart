288a8c76532a10bcf7079c9da661595444027cbb
lucky <>
2021-07-11 19:27:55 +0300

    refactor

--- lib/global.dart
+++ lib/global.dart
@@ -1,2 +1,3 @@
 const REACTOR_URL = 'http://joyreactor.cc';
-const USER_AGENT = 'Mozilla/5.0 (Android; Mobile; rv:68.0) Gecko/68.0 Firefox/89.0';
+const USER_AGENT =
+  'Mozilla/5.0 (Android; Mobile; rv:68.0) Gecko/68.0 Firefox/89.0';
